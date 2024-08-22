4f9b0887a8ae0e87a5ed9161cbac0d118645d00b
Herlyn <herlindavid269@gmail.com>
2024-08-22 16:29:57 -0500

    Terminando una nueva sección del curso de NodeJs

---
+++ src/presentation/todos/controller.ddd.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import { prisma } from "../../data/postgres";
+import { CreateTodoDto, TodoRepository, UpdateTodoDto } from "../../domain";
+
+export class TodosController {
+
+    //* DI
+    constructor(
+        private readonly todoRepository: TodoRepository,
+    ) { }
+
+    public getTodos = async (req: Request, res: Response) => {
+        const todos = await this.todoRepository.getAll();
+        return res.json(todos);
+    };
+
+    public getTodoById = async (req: Request, res: Response) => {
+        const id = +req.params.id;
+        
+        try {
+            const todo = await this.todoRepository.findById(id);
+            res.json(todo);
+        } catch (error) {
+            res.status(400).json({error});
+        }
+    };
+
+    public createTodo = async (req: Request, res: Response) => {
+        const [error, createTodoDto] = CreateTodoDto.create(req.body);
+        if (error) return res.status(400).json({ error });
+
+        const todo = await this.todoRepository.create(createTodoDto!);
+        res.json(todo);
+    };
+
+    public updateTodo = async (req: Request, res: Response) => {
+        const id = +req.params.id;
+        const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id });
+        if (error) return res.status(400).json({ error });
+
+        const updateTodo = await this.todoRepository.updateById(updateTodoDto!);
+        return res.json(updateTodo);
+    };
+
+    public deleteTodo = async (req: Request, res: Response) => {
+        const id = +req.params.id;
+        const deleteTodo = await this.todoRepository.deleteById(id);
+        res.json(deleteTodo);
+    };
+}
